ac52fa2bf9c9fbb1909017ed107e580cab31e881
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-03-12 17:30:51 +0700

    Add files via upload

---
+++ Lab22.2.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<list>
+#include<algorithm>
+
+using namespace std;
+
+void printList(list<string> s){
+	list<string>::iterator i;
+	for( i = s.begin(); i != s.end(); i++){
+		cout << *i << " ";
+	}		
+	cout << "\n";	
+}
+
+int main(){
+	
+	list<string> line_up;
+	list<string>::iterator loc;
+	
+	line_up.push_back("Alice");
+	line_up.push_back("Bob");
+	
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Oscar");
+	
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+	
+	loc = find(line_up.begin(),line_up.end(),"Sanji");
+	line_up.insert(loc,"Narutu");
+	
+	line_up.push_front("Prayath");
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.insert(loc,"Tony");
+	
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	line_up.erase(loc);
+	
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+	loc = line_up.begin();
+	line_up.erase(loc);
+	
+	
+	
+	
+	
+	printList(line_up);
+		
+	return 0;
+}
